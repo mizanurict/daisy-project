352bb99be9a9b2c6d0df0e0ef3c1e56a63535fe9
Mizanur_Rahman <mizanurictiu@gmail.com>
2024-01-21 03:20:32 +0600

    added slider and others

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,13 @@
 module.exports = {
     content: ["./src/**/*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+            clifford: '#da373d',
+            'bike-primary': '#E76F51',
+            'bike-primary-bg': 'rgba(231, 111, 81, 0.10)',
+          }
+      },
     },
     plugins: [],
   }
